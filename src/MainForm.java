c75002d3aef752bf06d86c05c319db648e4b06df
Dmitry Pyzh <dmitry.pyzh2gmail.com>
2013-03-01 19:16:05 +0600

    Release

--- src/MainForm.java
+++ src/MainForm.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.event.*;
+import java.io.*;
+import javax.swing.filechooser.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -18,7 +20,39 @@ public class MainForm {
 
     public MainForm() {
 
+        statusTextArea.setText("Выберите файл...");
 
+        openButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                final JFileChooser fc = new JFileChooser();
+                fc.setCurrentDirectory(new File(System.getProperty("user.dir")));
+                fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                fc.setAcceptAllFileFilterUsed(false);
+                fc.setMultiSelectionEnabled(false);
+                fc.addChoosableFileFilter(new FileNameExtensionFilter("Текстовый файл (*.txt)", "txt"));
+                int r = fc.showOpenDialog(mainPanel);
+                if (r == JFileChooser.APPROVE_OPTION) {
+                    try {
+                        FileInputStream stream = new FileInputStream(fc.getSelectedFile().getPath());
+                        InputStreamReader reader = new InputStreamReader(stream);
+                        int ch = reader.read();
+                        int count = 0;
+                        while (ch != -1) {
+                            if (ch != 13 && ch != 10)
+                                count++;
+                            ch = reader.read();
+                        }
+                        reader.close();
+                        stream.close();
+                        statusTextArea.setText(String.format("Файл: %s%n%nПечатных знаков (символов): %,d%n%nАвторских листов: %.2f", fc.getSelectedFile().getPath(), count, (double) count / 40000));
+                    } catch (Exception e) {
+                        statusTextArea.setText(String.format("Ошибка: %s", e.getMessage()));
+                    }
+                } else {
+                    statusTextArea.setText("Выберите файл...");
+                }
+            }
+        });
 
         closeButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -26,14 +60,12 @@ public class MainForm {
             }
         });
 
-
-
-
     }
 
     public static void main(String[] args) {
-        //
-        JFrame frame = new JFrame("MainForm");
+
+        JFrame frame = new JFrame("Подсчет количества символов");
+        frame.setResizable(false);
         frame.setContentPane(new MainForm().mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
