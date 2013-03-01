b9c646d968ccbc3afb58132725b918e8fa64ddce
Dmitry Pyzh <dmitry.pyzh2gmail.com>
2013-03-01 17:28:44 +0600

    Null

--- src/MainForm.java
+++ src/MainForm.java
@@ -32,7 +32,7 @@ public class MainForm {
     }
 
     public static void main(String[] args) {
-
+        //
         JFrame frame = new JFrame("MainForm");
         frame.setContentPane(new MainForm().mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
